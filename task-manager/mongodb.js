d52395541e3ff1b0799af5b50a0ea1d3a9ba92ae
Ammar Sayed <ammarsayed1988@gmail.com>
2022-09-01 00:36:43 +0200

    end of section 10

---
+++ task-manager/mongodb.js
@@ -0,0 +1,41 @@
+// CRUD Operations
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const uri =
+  "mongodb+srv://ammar:A123456e@cluster0.3foql77.mongodb.net/?retryWrites=true&w=majority";
+const databaseName = "task-manager";
+const collectionName = "users";
+
+const id = new ObjectId();
+console.log(id);
+console.log(id.getTimestamp());
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    const db = client.db(databaseName);
+
+    const result = await db
+      .collection(collectionName)
+      .findOne({ _id: new ObjectId("630f79f1162129d964d9ebdd") });
+
+    console.log(result);
+    const data = await db
+      .collection("tasks")
+      .find({ completed: false })
+      .toArray();
+
+    console.log(data.length);
+  } catch (error) {
+    return console.log("unable to connect to data base", error);
+  } finally {
+    await client.close();
+  }
+};
+
+run();
